3575555d4fcbf1bd03b6981fcb65ed458e64b6b8
Dmytro Frolov <dmytrofrolov@users.noreply.github.com>
2014-06-17 20:34:26 +0300

    Update -1.SolitaireMancala.py

--- -1.SolitaireMancala.py
+++ -1.SolitaireMancala.py
@@ -1,96 +1,97 @@
 #Don't forget about https://class.coursera.org/principlescomputing-001/wiki/honor_code
-#http://www.codeskulptor.org/#user34_YZPCJkIc44_14.py
-#zero project SolitaireMancala for principles of computing
-
+#http://www.codeskulptor.org/#user34_YZPCJkIc44_17.py
 
 """
-zero project SolitaireMancala for principles of computing
+double zero project SolitaireMancala for principles of computing
 """
 #zero project SolitaireMancala for principles of computing
 
 
 class SolitaireMancala:
     """
-    initialization function
+    SolitaireMancala class
     """
-    #initialization function
+    
     def __init__(self):
-        self.board = [0]
+        """
+        initialization function
+        """
+        self._board = [0]
     
-    """
-    setting the board
-    """
-    #setting the board
+
     def set_board(self, configuration):
-        self.board = list(configuration)
-        return self.board
+        """
+        setting the board
+        """
+        self._board = list(configuration)
+        return self._board
     
-    """
-    return bord like string in reverse order
-    """
-    #return bord like string in reverse order
+
     def __str__(self):
-        temp = list(self.board)
+        """
+        return bord like string in reverse order
+        """
+        temp = list(self._board)
         temp.reverse()
         return str(temp)
-    """
-    return number of seeds in seperate house
-    """
-    #return number of seeds in seperate house
+
     def get_num_seeds(self, house_num):
-        return self.board[house_num]
+        """
+        return number of seeds in seperate house
+        """
+        return self._board[house_num]
     
-    """
-    boolean function check is the move legal
-    """
-    #boolean function check is the move legal
+
     def is_legal_move(self, house_num):
+        """
+        boolean function check is the move legal
+        """
         if house_num == 0:
             return False   
         
-        if house_num == self.board[house_num]:
+        if house_num == self._board[house_num]:
             return True
         else:
             return False
     
-    """
-    applying the mive if it is legal
-    """
-    #applying the mive if it is legal    
+
     def apply_move(self, house_num):
+        """
+        applying the mive if it is legal
+        """
         if self.is_legal_move(house_num):
             for temp_index in range(0,house_num):
-                self.board[temp_index]+=1
-                self.board[house_num]-=1
+                self._board[temp_index]+=1
+                self._board[house_num]-=1
     
-    """
-    choose first legal move if it is illegal return 0
-    """
-    #choose first legal move if it is illegal return 0
+
     def choose_move(self):
-        for temp_index in range(1, len(self.board)):
-            if self.board[temp_index]==temp_index:
+        """
+        choose first legal move if it is illegal return 0
+        """
+        for temp_index in range(1, len(self._board)):
+            if self._board[temp_index]==temp_index:
                 return int(temp_index)
         return int(0)
     
-    """
-    check is the game if finished by winning
-    """
-    #check is the game if finished by winning
+
     def is_game_won(self):
-        for temp_index in range(1, len(self.board)):
-            if self.board[temp_index]!=0:
+        """
+        check is the game if finished by winning
+        """
+        for temp_index in range(1, len(self._board)):
+            if self._board[temp_index]!=0:
                 return False
         return True
       
-    """
-    plan the moves for winning
-    """  
-    #plan the moves for winning
+
     def plan_moves(self):
+        """
+        plan the moves for winning
+        """  
         plan_moves_list = []
         temp = SolitaireMancala()
-        temp.set_board(list(self.board))
+        temp.set_board(list(self._board))
         move = 0
         move = temp.choose_move()
         while move!=0:
@@ -99,3 +100,7 @@ class SolitaireMancala:
             move = temp.choose_move()
         return plan_moves_list
             
+            
+import poc_mancala_gui
+poc_mancala_gui.run_gui(SolitaireMancala())
+
